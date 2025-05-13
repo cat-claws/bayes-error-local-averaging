b0160652ede49c39587f70fd37d53a5dd7fbfb09
cat-claws <47313357+cat-claws@users.noreply.github.com>
2025-05-13 22:13:45 +0800

    Update x_gradient_autograd.py

--- bayeslap/_torch/x_gradient_autograd.py
+++ bayeslap/_torch/x_gradient_autograd.py
@@ -1,6 +1,6 @@
-def bayes_error_grad_torch_autograd(X, y, sigma, num_classes):
+def bayes_error_grad_torch_autograd(f, X, y, sigma, num_classes):
 	X = X.clone().detach().requires_grad_(True)
-	losses = bayes_error(X, y, sigma, num_classes)
+	losses = f(X, y, sigma, num_classes)
 	loss = losses.sum()
 	loss.backward()
 	return X.grad.detach()
