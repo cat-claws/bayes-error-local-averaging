a8abd52a60abfd8b4f4748277d9cc979a49cb0f8
cat-claws <47313357+cat-claws@users.noreply.github.com>
2025-05-14 00:45:42 +0800

    Update __init__.py

--- bayeslap/__init__.py
+++ bayeslap/__init__.py
@@ -1,2 +1,3 @@
-from bayeslap._numpy.bayes_error_rbf_kernel import bayes_error_numpy
-from bayeslap._torch.bayes_error_rbf_kernel import bayes_error_torch
+# from bayeslap._numpy.bayes_error_rbf_kernel import bayes_error_numpy
+# from bayeslap._torch.bayes_error_rbf_kernel import bayes_error_torch
+from ._torch.bayes_error_rbf_kernel_autograd_function import BayesErrorRBF
