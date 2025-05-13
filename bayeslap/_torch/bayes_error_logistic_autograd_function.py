bf68fcc5e1b7df8ccb45b10f2fa36d4655edef72
cat-claws <47313357+cat-claws@users.noreply.github.com>
2025-05-14 01:47:26 +0800

    Update bayes_error_logistic_autograd_function.py

--- bayeslap/_torch/bayes_error_logistic_autograd_function.py
+++ bayeslap/_torch/bayes_error_logistic_autograd_function.py
@@ -7,7 +7,8 @@ class BayesErrorLogistic(Function):
 	def forward(ctx, X, y, num_classes, chunk_size=512):
 		N, D = X.shape
 		grads = torch.zeros_like(X)
-		total_error = 0.0
+		# total_error = 0.0
+		posteriors = torch.zeros(N, num_classes, device=X.device)
 	
 		for start in trange(0, N, chunk_size, desc="Forward"):
 			end = min(start + chunk_size, N)
@@ -28,12 +29,12 @@ class BayesErrorLogistic(Function):
 			Z = sims.sum(dim=1, keepdim=True) + 1e-8  # [B, 1]
 	
 			# Posteriors
-			posteriors = torch.zeros(B, num_classes, device=X.device)
+			# posteriors = torch.zeros(B, num_classes, device=X.device)
 			for c in range(num_classes):
 				class_mask = (y == c).float()  # [N]
-				posteriors[:, c] = (sims * class_mask).sum(dim=1) / Z.squeeze(1)
+				posteriors[start:end, c] = (sims * class_mask).sum(dim=1) / Z.squeeze(1)
 	
-			y_max = torch.argmax(posteriors, dim=1)  # [B]
+			y_max = torch.argmax(posteriors[start:end], dim=1)  # [B]
 			y_true = y_chunk
 			total_error += (y_max != y_true).float().sum().item()
 	
@@ -51,7 +52,8 @@ class BayesErrorLogistic(Function):
 			grads -= grad_j
 	
 		ctx.save_for_backward(grads)
-		return torch.tensor(total_error / N, device=X.device, dtype=X.dtype)
+		# return torch.tensor(total_error / N, device=X.device, dtype=X.dtype)
+		return (1.0 - posteriors.max(dim=1).values).mean()
 	
 	@staticmethod
 	def backward(ctx, grad_output):
